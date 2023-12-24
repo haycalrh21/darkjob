9add44629e4400398e34bef058b546668a274a48
Bhazyga <bhazyacyuta@gmail.com>
2023-12-24 11:25:50 +0700

    update minggu

--- src/Template/Header.js
+++ src/Template/Header.js
@@ -8,15 +8,18 @@ const Header = ({ scrollToLayanan, scrollToKelasIT }) => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
+  const images = ['inioke.png'];
+
+
   return (
     <header className='' style={{ background: 'linear-gradient(41deg, rgba(158,158,158,1) 55%, rgba(193,176,127,1) 67%, rgba(200,182,132,1) 71%, rgba(203,187,141,1) 76%, rgba(158,158,158,1) 79%)', color: '#DCD7C9', padding: '10px' }}>
-      <img src='inioke.png' style={{ width: 100 }} alt="Logo" />
+      <img src={images[0]} style={{ width: 100 }} alt="Logo" />
 
       {/* Hamburger menu button for mobile */}
 
 
       {/* Desktop navigation */}
-      <div style={{ position: 'absolute', top: '40px', right: '20px', display: 'block' }}>
+      {/* <div style={{ position: 'absolute', top: '40px', right: '20px', display: 'block' }}>
         <button className=''>
           <input
             className='rounded-lg'
@@ -24,10 +27,10 @@ const Header = ({ scrollToLayanan, scrollToKelasIT }) => {
             placeholder="Search"
           />
           <svg >
-            {/* <path style={{ color: 'darkblue' }} d="M120.386 15.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z" stroke="currentColor" fill="none" strokeWidth="2" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round"></path> */}
+            <path style={{ color: 'darkblue' }} d="M120.386 15.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z" stroke="currentColor" fill="none" strokeWidth="2" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round"></path>
           </svg>
         </button>
-      </div>
+      </div> */}
 
 
 
